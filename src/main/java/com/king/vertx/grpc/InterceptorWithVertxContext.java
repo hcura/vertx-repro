149ca88e016de9f6a8ac7d9efdf62a1f3489347a
Hugo Cura <hvieira.cura@gmail.com>
2025-03-27 13:29:11 +0000

    test

--- src/main/java/com/king/vertx/grpc/InterceptorWithVertxContext.java
+++ src/main/java/com/king/vertx/grpc/InterceptorWithVertxContext.java
@@ -1,6 +1,5 @@
 package com.king.vertx.grpc;
 
-import io.grpc.ForwardingServerCallListener;
 import io.grpc.Metadata;
 import io.grpc.ServerCall;
 import io.grpc.ServerCallHandler;
@@ -13,32 +12,10 @@ public class InterceptorWithVertxContext implements ServerInterceptor {
 
     @Override
     public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call, Metadata headers, ServerCallHandler<ReqT, RespT> next) {
-        addToContext();
-
-        return new ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT>(next.startCall(call, headers)) {
-            @Override
-            public void onMessage(ReqT message) {
-//                addToContext();
-                super.onMessage(message);
-            }
-
-            @Override
-            public void onHalfClose() {
-//                addToContext();
-                super.onHalfClose();
-            }
-
-            @Override
-            public void onReady() {
-//                addToContext();
-                super.onReady();
-            }
-        };
-    }
-
-    private static void addToContext() {
         var currentContext = Vertx.currentContext().getLocal(CUSTOM_CONTEXT_LOCAL, CurrentContext::new);
         currentContext.put(String.class, "vertx");
+
+        return next.startCall(call, headers);
     }
 
 }
